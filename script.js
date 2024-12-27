fe335f44b9a357b24208699d7098d153760dbcba
Luka2006 <lukaoqropiridze@gmail.com>
2024-12-27 14:10:27 +0400

    javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+// cvladebi
+let Name = "Luka";
+let Lastname = "oqropiridze";
+let Age = 18;
+let Email = "lukaoqropiridze9@gmail.com";
+let Number = "568946233";
+let Adult = true;
+
+// ganaxleba
+function New_Age(New_Age) {
+  Age = New_Age;
+}
+
+// console
+console.log("Name:", Name);
+console.log("Lastname:", Lastname);
+console.log("Age:", Age);
+console.log("Email:", Email);
+console.log("%c Adult: " + Adult, "color: yellow;");
+console.log("%c Number: " + Number, "color: red;");
